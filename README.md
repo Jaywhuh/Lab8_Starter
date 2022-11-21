2c9af12ff552e34f7bd464ed292f184f66bfa853
Jinwoong Huh <jaywhuh@gmail.com>
2022-11-20 20:57:23 -0800

    Finished Explore

--- README.md
+++ README.md
@@ -11,6 +11,15 @@ Answer: I would fit my automated tests in my Recipe project within a Github acti
 *Question 2. Would you use an end to end test to check if a function is returning the correct output? (yes/no)*
 
 Answer: No I would not use an end to end test to check if a function is returning the correct output because an end to end test is supposed to emulate user actions in order to determine if the program runs smoothly. Unit Testing would be much for suitable to check if a function is return the correct output or not. 
+<br>
+<br>
 
+*Question 3. Would you use a unit test to test the “message” feature of a messaging application? Why or why not? For this question, assume the “message” feature allows a user to write and send a message to another user.*
 
+Answer: No I would not use a unit test because the message feature requires user interaction, which is not suitable to test with unit testing. Unit testing is more suitable for testing specific code functionalities. 
+<br>
+<br>
 
+*Question 4. Would you use a unit test to test the “max message length” feature of a messaging application? Why or why not? For this question, assume the “max message length” feature prevents the user from typing more than 80 characters.*
+
+Answer: Yes I would use a unit test to test this functionality because it only checks a specific funtionality of the code that prevents the user from typing more than 80 characters and doesn't overlap much with other functionalities. 
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -3,3 +3,91 @@
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+// Testing Phone Numbers
+test('valid phone number', () => {
+    expect(functions.isPhoneNumber('(123)-123-1234')).toBe(true);
+});
+
+test('valid phone number', () => {
+    expect(functions.isPhoneNumber('123-123-1234')).toBe(true);
+});
+
+test('invalid phone number', () => {
+    expect(functions.isPhoneNumber('1231234')).toBe(false);
+});
+
+test('invalid phone number', () => {
+    expect(functions.isPhoneNumber('(123)-123')).toBe(false);
+});
+
+
+//Testing Email addresses
+test('valid email address', () => {
+    expect(functions.isEmail('helloWorld@gmail.com')).toBe(true);
+});
+
+test('valid email address', () => {
+    expect(functions.isEmail('1234Testtest@yahoo.com')).toBe(true);
+});
+
+test('invalid email address', () => {
+    expect(functions.isEmail('1234Testtest@testing')).toBe(false);
+});
+
+test('invalid email address', () => {
+    expect(functions.isEmail('!#$@test.com')).toBe(false);
+});
+
+
+//Testing Password
+test('strong password', () => {
+    expect(functions.isStrongPassword('password1234')).toBe(true);
+});
+
+test('strong password', () => {
+    expect(functions.isStrongPassword('lab8_')).toBe(true);
+});
+
+test('weak password', () => {
+    expect(functions.isStrongPassword('wow')).toBe(false);
+});
+
+test('weak password', () => {
+    expect(functions.isStrongPassword('whyisthispasswordtolong')).toBe(false);
+});
+
+
+//Testing date
+test('valid date format', () => {
+    expect(functions.isDate('12/31/2022')).toBe(true);
+});
+
+test('valid date format', () => {
+    expect(functions.isDate('1/1/2022')).toBe(true);
+});
+
+test('invalid date format', () => {
+    expect(functions.isDate('12 / 12 / 22')).toBe(false);
+});
+
+test('invalid date format', () => {
+    expect(functions.isDate('November 20th, 2022')).toBe(false);
+});
+
+
+//Testing Hex Color
+test('valid hex code', () => {
+    expect(functions.isHexColor('aef093')).toBe(true);
+});
+
+test('valid hex code', () => {
+    expect(functions.isHexColor('5e50fa')).toBe(true);
+});
+
+test('invalid hex code', () => {
+    expect(functions.isHexColor('test')).toBe(false);
+});
+
+test('invalid hex code', () => {
+    expect(functions.isHexColor('23jk!3')).toBe(false);
+});
